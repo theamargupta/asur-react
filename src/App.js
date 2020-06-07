87c177dfbc41a702d75e1e531f63242cd789d227
Amar Gupta <amar9911056196@gmail.com>
2020-06-07 17:59:16 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React from "react";
 import "./App.css";
 
-import name from './components/name/name'
-import {CardList} from "./components/card-list/ca-li-com"
+import name from "./components/name/name";
+import { CardList } from "./components/card-list/ca-li-com";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      asurs: name
+      asurs: name,
     };
   }
 
   render() {
     return (
       <div className="App">
-      <CardList asurs={this.state.asurs} />
+        <input
+          type="search"
+          placeholder="search asur"
+          onChange={ e => {
+            this.setState({ searchField: e.target.value }
+            );
+          }}
+        />
+        <img
+          src="https://img.icons8.com/pastel-glyph/64/000000/search--v2.png"
+          alt=""
+        />
+        <CardList asurs={this.state.asurs} />
       </div>
     );
   }
 }
 export default App;
+//23 video
