cc3f799146017fc1d22d81c5e5dae8dbefd269c8
Amar Gupta <amar9911056196@gmail.com>
2020-06-09 00:23:29 +0530

    Updates

---
+++ precache-manifest.be5e3c80835d2912a5d8bee4b62733d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6590e07cbb5070fde0f01bc60a67cf1d",
+    "url": "/asur-react/index.html"
+  },
+  {
+    "revision": "3dd7b71707467a39a2b0",
+    "url": "/asur-react/static/css/main.b96d4aec.chunk.css"
+  },
+  {
+    "revision": "74e93f2aa198f1e313f2",
+    "url": "/asur-react/static/js/2.a82c8ca0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/asur-react/static/js/2.a82c8ca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dd7b71707467a39a2b0",
+    "url": "/asur-react/static/js/main.7a0ff7b8.chunk.js"
+  },
+  {
+    "revision": "fa8c3daeca32dc0385ef",
+    "url": "/asur-react/static/js/runtime-main.d39cd26c.js"
+  }
+]);
